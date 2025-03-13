2bb9c5405b72295a58d759d4ab2809808cc2fef6
Thang Nguyen <unicodeee2000@gmail.com>
2025-03-12 21:33:58 -0700

    fix seedmine might overlapped 1 mine multiple time when seed randomly (fixed)

--- src/mineField/MineField.java
+++ src/mineField/MineField.java
@@ -3,17 +3,17 @@ package mineField;
 import mvc.Model;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class MineField extends Model {
 
-    private final int gridViewSize = 20;  // grid to be displayed is 20 x 20
+    private final int gridViewSize = 3;  // grid to be displayed is 20 x 20
     private final int actualGrid = gridViewSize + 1; // design choice to handle check mines
-    private final int percentMined = 100;
+    private final int percentMined = 50;
     private final int mineAmount = gridViewSize * gridViewSize * percentMined / 100;
-    private List<Point> mines = new ArrayList<>(mineAmount );
+    private Set<Point> mines = new HashSet<>(mineAmount );
     private final boolean steppedOnMine = true;
 
 
@@ -33,7 +33,7 @@ public class MineField extends Model {
         return gridViewSize;
     }
 
-    public List<Point> getMines() {
+    public Set<Point> getMines() {
         return mines;
     }
 
@@ -42,13 +42,30 @@ public class MineField extends Model {
         seedMines();
     }
 
+    public Point seedMine() {
+        Random random = new Random();
+        int row = random.nextInt(gridViewSize ) + 1;
+        int col = random.nextInt(gridViewSize ) + 1;
+        Point p = new Point(row, col);
+
+
+        while (mines.contains(p)) { // handle overlapped coordinates
+            p.x += 1;
+            if (p.x > gridViewSize) {
+                p.x = p.x % gridViewSize;
+                p.y += 1;
+                if (p.y > gridViewSize) {
+                    p.y = p.y % gridViewSize;
+                }
+            }
+        }
+        return p;
+    }
+
     public void seedMines() {
         mines.clear();
         for (int i = 0; i < mineAmount; i++) {
-            Random random = new Random();
-            int row = random.nextInt(gridViewSize ) + 1;
-            int col = random.nextInt(gridViewSize ) + 1;
-            mines.add(new Point(row, col));
+            mines.add(seedMine());
         }
     }
 
