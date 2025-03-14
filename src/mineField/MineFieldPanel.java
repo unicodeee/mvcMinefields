78cf8e659395185bb708258587d658fb7bb8408e
tofu <isabelleluu0w0@gmail.com>
2025-03-13 19:57:02 -0700

    rebase from isabelle-luu

--- src/mineField/MineFieldPanel.java
+++ src/mineField/MineFieldPanel.java
@@ -8,50 +8,46 @@ import java.awt.*;
 
 public class MineFieldPanel extends AppPanel {
 
-
-    private JButton nwButton;
-    private JButton nButton;
-    private JButton neButton;
-    private JButton wButton;
-    private JButton eButton;
-    private JButton swButton;
-    private JButton sButton;
-    private JButton seButton;
+    private JButton northwest;
+    private JButton north;
+    private JButton northeast;
+    private JButton west;
+    private JButton east;
+    private JButton southwest;
+    private JButton south;
+    private JButton southeast;
 
     public MineFieldPanel(AppFactory factory) {
         super(factory);
-
-        nwButton = new JButton("NW");
-        nButton = new JButton("N");
-        neButton = new JButton("NE");
-        wButton = new JButton("W");
-        eButton = new JButton("E");
-        swButton = new JButton("SW");
-        sButton = new JButton("S");
-        seButton = new JButton("SE");
-
-        nwButton.addActionListener(this);
-        nButton.addActionListener(this);
-        neButton.addActionListener(this);
-        wButton.addActionListener(this);
-        eButton.addActionListener(this);
-        swButton.addActionListener(this);
-        sButton.addActionListener(this);
-        seButton.addActionListener(this);
+        northwest = new JButton("NW");
+        north = new JButton("N");
+        northeast = new JButton("NE");
+        west = new JButton("W");
+        east = new JButton("E");
+        southwest = new JButton("SW");
+        south = new JButton("S");
+        southeast = new JButton("SE");
+        northwest.addActionListener(this);
+        north.addActionListener(this);
+        northeast.addActionListener(this);
+        west.addActionListener(this);
+        east.addActionListener(this);
+        southwest.addActionListener(this);
+        south.addActionListener(this);
+        southeast.addActionListener(this);
 
         // Add buttons (optionally add action listeners)
-        controlPanel.add(nwButton);
-        controlPanel.add(nButton);
-        controlPanel.add(neButton);
-        controlPanel.add(wButton);
-        controlPanel.add(eButton);
-        controlPanel.add(swButton);
-        controlPanel.add(sButton);
-        controlPanel.add(seButton);
+        controlPanel.add(northwest);
+        controlPanel.add(north);
+        controlPanel.add(northeast);
+        controlPanel.add(west);
+        controlPanel.add(east);
+        controlPanel.add(southwest);
+        controlPanel.add(south);
+        controlPanel.add(southeast);
 
         // Set layout for the control panel
         controlPanel.setLayout(new GridLayout(4, 2));
-
     }
 
     public static void main(String[] args) {
